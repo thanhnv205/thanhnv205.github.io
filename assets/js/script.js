3314f37255b1050e7413101241fcf0f8bcb3923b
thanhnv205 <stcorndev@gmail.com>
2025-01-03 15:16:20 +0700

    modify game

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,246 +1,430 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const gameArea = document.getElementById('gameArea')
-  const gun = document.getElementById('gun')
-  const ammoContainer = document.getElementById('ammo')
-  const overlay = document.getElementById('overlay')
-  const messageBox = document.getElementById('messageBox')
+//Vanilla JS
 
-  let score = 0
-  let bulletsLeft = 10
+//PLAY IN FULL PAGE VIEW!
 
-  const timeDrawChicken = 2000
-  const reloadTime = 3000
-  const maxBullets = 10
-  const chickens = []
 
-  // Tạo băng đạn
-  const drawBulletBelt = () => {
-    for (let i = 0; i < maxBullets; i++) {
-      const indicator = document.createElement('span')
-      indicator.classList.add('bullet-indicator', 'active')
-      ammoContainer.appendChild(indicator)
-    }
-  }
-  drawBulletBelt()
-  const bulletBelt = document.querySelectorAll('.bullet-indicator')
+window.addEventListener("DOMContentLoaded", game);
 
-  const updateScore = () => {
-    document.getElementById('score').innerText = `Score: ${score}`
-  }
+//General sprite load
+var sprite = new Image();
+var spriteExplosion = new Image();
+sprite.src = 'https://marclopezavila.github.io/planet-defense-game/img/sprite.png';
 
-  const createChicken = () => {
-    const chicken = document.createElement('div')
-    chicken.classList.add('chicken')
+window.onload = function () {
+  spriteExplosion.src = 'https://marclopezavila.github.io/planet-defense-game/img/explosion.png';
+};
 
-    // Vị trí ngẫu nhiên cho con gà
-    const randomTop = Math.random() * (gameArea.offsetHeight - 250) + 50
-    chicken.style.top = `${randomTop}px`
+//Game
+function game() {
 
-    // Kích thước tối đa 
-    const maxWidth = 100
-    const maxHeight = 80
-    // Kích thước  tối thiểu
-    const minWidth = 80
-    const minHeight = 60
+  //Canvas
+  var canvas = document.getElementById('canvas'),
+    ctx = canvas.getContext('2d'),
+    cH = ctx.canvas.height = window.innerHeight,
+    cW = ctx.canvas.width = window.innerWidth;
 
-    // Kích thước chuẩn khi con gà ở giữa
-    const middleY = gameArea.offsetHeight / 2
-    const baseWidth = 60
-    const baseHeight = 40
+  //Game
+  var bullets = [],
+    asteroids = [],
+    explosions = [],
+    destroyed = 0,
+    record = 0,
+    count = 0,
+    playing = false,
+    gameOver = false,
+    _planet = { deg: 0 };
 
-    // Tính tỷ lệ kích thước dựa trên vị trí ngẫu nhiên
-    const sizeRatio = Math.abs(randomTop - middleY) / (gameArea.offsetHeight / 2)
+  //Player
+  var player = {
+    posX: -35,
+    posY: -(100 + 82),
+    width: 70,
+    height: 79,
+    deg: 0
+  };
 
-    // Không nhỏ hơn minWidth, nhỏ hơn minHeight
-    const chickenHeight = Math.max(minHeight, baseHeight + (maxHeight - baseHeight) * (1 - sizeRatio))
-    const chickenWidth = Math.max(minWidth, baseWidth + (maxWidth - baseWidth) * (1 - sizeRatio))
+  canvas.addEventListener('click', action);
+  canvas.addEventListener('mousemove', action);
+  window.addEventListener("resize", update);
 
-    // Thêm hình ảnh con gà và áp dụng kích thước
-    const chickenImage = document.createElement('img')
-    const direction = Math.random() < 0.5 ? 'left' : 'right'   // Chọn hướng bay
-    chickenImage.src = direction === 'left' ? './assets/images/chip-left.png' : './assets/images/chicken-right.png'
-
-    // Áp dụng kích thước cho hình ảnh con gà
-    chickenImage.style.width = `${chickenWidth}px`
-    chickenImage.style.height = `${chickenHeight}px`
-
-    chicken.appendChild(chickenImage)
-
-    // Xác định hướng bay của con gà
-    const animationDirection = direction === 'left' ? 'fly-to-left' : 'fly-to-right'  // Lựa chọn animation dựa trên hướng
-    chicken.style.animation = `${animationDirection} 8s linear`
-
-    // Thêm con gà vào gameArea và vào mảng chickens
-    gameArea.appendChild(chicken)
-    chickens.push(chicken)
-
-    // Xử lý khi animation kết thúc
-    chicken.addEventListener('animationend', () => {
-      chicken.remove()
-      const index = chickens.indexOf(chicken)
-      if (index > -1) chickens.splice(index, 1) // Con gà đã qua khung hình
-    })
+  function update() {
+    cH = ctx.canvas.height = window.innerHeight;
+    cW = ctx.canvas.width = window.innerWidth;
   }
 
-  const showMessage = (msg) => {
-    messageBox.innerText = msg
-    overlay.style.display = 'flex'
+  function move(e) {
+    player.deg = Math.atan2(e.offsetX - (cW / 2), -(e.offsetY - (cH / 2)));
   }
 
-  const hideOverlay = () => {
-    overlay.style.display = 'none'
-  }
+  function action(e) {
+    e.preventDefault();
+    if (playing) {
+      var bullet = {
+        x: -8,
+        y: -179,
+        sizeX: 2,
+        sizeY: 10,
+        realX: e.offsetX,
+        realY: e.offsetY,
+        dirX: e.offsetX,
+        dirY: e.offsetY,
+        deg: Math.atan2(e.offsetX - (cW / 2), -(e.offsetY - (cH / 2))),
+        destroyed: false
+      };
 
-  // Tạo gà theo thời gian
-  let lastChickenTime = 0
-  const handleChickenCreation = (timestamp) => {
-    // 3s cho mỗi con gà
-    if (timestamp - lastChickenTime >= timeDrawChicken) {
-      createChicken()
-      lastChickenTime = timestamp
-    }
-    requestAnimationFrame(handleChickenCreation)
-  }
-
-  requestAnimationFrame(handleChickenCreation)
-
-  gameArea.addEventListener('click', (event) => {
-    if (bulletsLeft > 0) {
-      bulletsLeft--
-      updateBulletBelt()
-      fireBullet(event)
+      bullets.push(bullet);
     } else {
-      showMessage('Out of bullets! Reloading...')
-      setTimeout(() => {
-        bulletsLeft = maxBullets
-        updateBulletBelt()
-        console.log('Reloaded!')
-        hideOverlay()
-      }, reloadTime)
-    }
-  })
-
-  const fireBullet = (event) => {
-    const bullet = document.createElement('div')
-    bullet.classList.add('bullet')
-    gameArea.appendChild(bullet)
-
-    const targetX = event.clientX - gameArea.getBoundingClientRect().left
-    const targetY = event.clientY - gameArea.getBoundingClientRect().top
-
-    const gunRect = gun.getBoundingClientRect()
-    const gunCenterX = gunRect.left + gunRect.width / 2 - gameArea.getBoundingClientRect().left
-    const gunBottomY = gunRect.bottom - gameArea.getBoundingClientRect().top
-
-    bullet.style.left = `${gunCenterX - 5}px`
-    bullet.style.top = `${gunBottomY - 5}px`
-    bullet.style.display = 'block'
-
-    const velocityX = targetX - gunCenterX
-    const velocityY = targetY - gunBottomY
-    const distance = Math.sqrt(velocityX * velocityX + velocityY * velocityY)
-    const duration = (distance / 100) * 0.05
-
-    bullet.style.transition = `transform ${duration}s linear`
-    bullet.style.transform = `translate(${velocityX}px, ${velocityY}px)`
-
-    bullet.addEventListener('transitionend', () => {
-      explodeBullet(targetX - 5, targetY - 5)
-      bullet.remove()
-    })
-
-    // Kiểm tra va chạm với gà
-    function checkCollision() {
-      const bulletRect = bullet.getBoundingClientRect()
-      chickens.forEach((chicken) => {
-        const chickenRect = chicken.getBoundingClientRect()
-
-        if (
-          bulletRect.left < chickenRect.right &&
-          bulletRect.right > chickenRect.left &&
-          bulletRect.top < chickenRect.bottom &&
-          bulletRect.bottom > chickenRect.top
-        ) {
-          const chickenCenterX = (chickenRect.left - 505) + chickenRect.width / 2
-          const chickenCenterY = (chickenRect.top - 210) + chickenRect.height / 2
-          explodeBullet(chickenCenterX, chickenCenterY)
-          chicken.remove()
-          bullet.remove()
-          score++
-          updateScore()
+      var dist;
+      if (gameOver) {
+        dist = Math.sqrt(((e.offsetX - cW / 2) * (e.offsetX - cW / 2)) + ((e.offsetY - (cH / 2 + 45 + 22)) * (e.offsetY - (cH / 2 + 45 + 22))));
+        if (dist < 27) {
+          if (e.type == 'click') {
+            gameOver = false;
+            count = 0;
+            bullets = [];
+            asteroids = [];
+            explosions = [];
+            destroyed = 0;
+            player.deg = 0;
+            canvas.removeEventListener('contextmenu', action);
+            canvas.removeEventListener('mousemove', move);
+            canvas.style.cursor = "default";
+          } else {
+            canvas.style.cursor = "pointer";
+          }
+        } else {
+          canvas.style.cursor = "default";
         }
-      })
-    }
+      } else {
+        dist = Math.sqrt(((e.offsetX - cW / 2) * (e.offsetX - cW / 2)) + ((e.offsetY - cH / 2) * (e.offsetY - cH / 2)));
 
-    // Kiểm tra va chạm liên tục với requestAnimationFrame
-    function collisionLoop() {
-      checkCollision()
-      if (bullet.parentElement) {
-        requestAnimationFrame(collisionLoop)
+        if (dist < 27) {
+          if (e.type == 'click') {
+            playing = true;
+            canvas.removeEventListener("mousemove", action);
+            canvas.addEventListener('contextmenu', action);
+            canvas.addEventListener('mousemove', move);
+            canvas.setAttribute("class", "playing");
+            canvas.style.cursor = "default";
+          } else {
+            canvas.style.cursor = "pointer";
+          }
+        } else {
+          canvas.style.cursor = "default";
+        }
       }
     }
-    collisionLoop()
   }
 
-  function explodeBullet(x, y) {
-    const explosion = document.createElement('div')
-    explosion.classList.add('explode')
-    explosion.style.left = `${x}px`
-    explosion.style.top = `${y}px`
-    explosion.style.position = 'absolute'
-    explosion.style.width = '10px'
-    explosion.style.height = '10px'
-    explosion.style.backgroundColor = 'red'
-    explosion.style.borderRadius = '50%'
-    gameArea.appendChild(explosion)
-    explosion.addEventListener('animationend', () => explosion.remove())
-  }
+  function fire() {
+    var distance;
 
-  function updateBulletBelt() {
-    bulletBelt.forEach((bullet, index) => {
-      bullet.classList.toggle('active', index < bulletsLeft)
-    })
-  }
+    for (var i = 0; i < bullets.length; i++) {
+      if (!bullets[i].destroyed) {
+        ctx.save();
+        ctx.translate(cW / 2, cH / 2);
+        ctx.rotate(bullets[i].deg);
 
-  updateBulletBelt()
+        ctx.drawImage(
+          sprite,
+          211,
+          100,
+          50,
+          75,
+          bullets[i].x,
+          bullets[i].y -= 20,
+          19,
+          30
+        );
 
-  // Di chuyển và quay đầu súng khi chuột di chuyển
-  const gunHead = document.getElementById('gun-head')
+        ctx.restore();
 
-  gameArea.addEventListener('mousemove', (event) => {
-    const gameAreaRect = gameArea.getBoundingClientRect()
-    const mouseX = event.clientX - gameAreaRect.left
-    const gameAreaWidth = gameAreaRect.width
+        //Real coords
+        bullets[i].realX = (0) - (bullets[i].y + 10) * Math.sin(bullets[i].deg);
+        bullets[i].realY = (0) + (bullets[i].y + 10) * Math.cos(bullets[i].deg);
 
-    // Tính toán trung tâm của thân súng
-    const gunRect = gun.getBoundingClientRect();
-    const gunCenterX = gunRect.left + gunRect.width;
+        bullets[i].realX += cW / 2;
+        bullets[i].realY += cH / 2;
 
-    // Tính độ lệch giữa chuột và trung tâm của thân súng
-    const deltaX = mouseX - gunCenterX
+        //Collision
+        for (var j = 0; j < asteroids.length; j++) {
+          if (!asteroids[j].destroyed) {
+            distance = Math.sqrt(Math.pow(
+              asteroids[j].realX - bullets[i].realX, 2) +
+              Math.pow(asteroids[j].realY - bullets[i].realY, 2)
+            );
 
-    const maxAngle = 90 // Góc quay tối đa
-    const angleY = (deltaX / gameAreaRect.width) * maxAngle * 2  // Tính góc quay từ -90 đến 90 độ
-
-    let rotate = angleY
-
-    const angleMapping = [
-      { min: -110, max: -90, value: -118 },
-      { min: -90, max: -70, value: 48 }
-    ];
-
-    for (const range of angleMapping) {
-      if (angleY >= range.min && angleY <= range.max) {
-        rotate = range.value;
-        break
+            if (distance < (((asteroids[j].width / asteroids[j].size) / 2) - 4) + ((19 / 2) - 4)) {
+              destroyed += 1;
+              asteroids[j].destroyed = true;
+              bullets[i].destroyed = true;
+              explosions.push(asteroids[j]);
+            }
+          }
+        }
       }
     }
-    gunHead.style.transform = `rotateY(${rotate}deg)`
+  }
 
-    // Scale the movement to slow it down (e.g., 0.4 slows the background)
-    const speedFactor = 0.2
+  function planet() {
+    ctx.save();
+    ctx.fillStyle = 'white';
+    ctx.shadowBlur = 100;
+    ctx.shadowOffsetX = 0;
+    ctx.shadowOffsetY = 0;
+    ctx.shadowColor = "#999";
 
-    const backgroundPosition = ((mouseX / gameAreaWidth) * 100 - 50) * speedFactor
-    gameArea.style.backgroundPosition = `${backgroundPosition}% center`
-  })
-})
+    ctx.arc(
+      (cW / 2),
+      (cH / 2),
+      100,
+      0,
+      Math.PI * 2
+    );
+    ctx.fill();
+
+    //Planet rotation
+    ctx.translate(cW / 2, cH / 2);
+    ctx.rotate((_planet.deg += 0.1) * (Math.PI / 180));
+    ctx.drawImage(sprite, 0, 0, 200, 200, -100, -100, 200, 200);
+    ctx.restore();
+  }
+
+  function _player() {
+
+    ctx.save();
+    ctx.translate(cW / 2, cH / 2);
+
+    ctx.rotate(player.deg);
+    ctx.drawImage(
+      sprite,
+      200,
+      0,
+      player.width,
+      player.height,
+      player.posX,
+      player.posY,
+      player.width,
+      player.height
+    );
+
+    ctx.restore();
+
+    if (bullets.length - destroyed && playing) {
+      fire();
+    }
+  }
+
+  function newAsteroid() {
+
+    var type = random(1, 4),
+      coordsX,
+      coordsY;
+
+    switch (type) {
+      case 1:
+        coordsX = random(0, cW);
+        coordsY = 0 - 150;
+        break;
+      case 2:
+        coordsX = cW + 150;
+        coordsY = random(0, cH);
+        break;
+      case 3:
+        coordsX = random(0, cW);
+        coordsY = cH + 150;
+        break;
+      case 4:
+        coordsX = 0 - 150;
+        coordsY = random(0, cH);
+        break;
+    }
+
+    var asteroid = {
+      x: 278,
+      y: 0,
+      state: 0,
+      stateX: 0,
+      width: 134,
+      height: 123,
+      realX: coordsX,
+      realY: coordsY,
+      moveY: 0,
+      coordsX: coordsX,
+      coordsY: coordsY,
+      size: random(1, 3),
+      deg: Math.atan2(coordsX - (cW / 2), -(coordsY - (cH / 2))),
+      destroyed: false
+    };
+    asteroids.push(asteroid);
+  }
+
+  function _asteroids() {
+    var distance;
+
+    for (var i = 0; i < asteroids.length; i++) {
+      if (!asteroids[i].destroyed) {
+        ctx.save();
+        ctx.translate(asteroids[i].coordsX, asteroids[i].coordsY);
+        ctx.rotate(asteroids[i].deg);
+
+        ctx.drawImage(
+          sprite,
+          asteroids[i].x,
+          asteroids[i].y,
+          asteroids[i].width,
+          asteroids[i].height,
+          -(asteroids[i].width / asteroids[i].size) / 2,
+          asteroids[i].moveY += 1 / (asteroids[i].size),
+          asteroids[i].width / asteroids[i].size,
+          asteroids[i].height / asteroids[i].size
+        );
+
+        ctx.restore();
+
+        //Real Coords
+        asteroids[i].realX = (0) - (asteroids[i].moveY + ((asteroids[i].height / asteroids[i].size) / 2)) * Math.sin(asteroids[i].deg);
+        asteroids[i].realY = (0) + (asteroids[i].moveY + ((asteroids[i].height / asteroids[i].size) / 2)) * Math.cos(asteroids[i].deg);
+
+        asteroids[i].realX += asteroids[i].coordsX;
+        asteroids[i].realY += asteroids[i].coordsY;
+
+        //Game over
+        distance = Math.sqrt(Math.pow(asteroids[i].realX - cW / 2, 2) + Math.pow(asteroids[i].realY - cH / 2, 2));
+        if (distance < (((asteroids[i].width / asteroids[i].size) / 2) - 4) + 100) {
+          gameOver = true;
+          playing = false;
+          canvas.addEventListener('mousemove', action);
+        }
+      } else if (!asteroids[i].extinct) {
+        explosion(asteroids[i]);
+      }
+    }
+
+    if (asteroids.length - destroyed < 10 + (Math.floor(destroyed / 6))) {
+      newAsteroid();
+    }
+  }
+
+  function explosion(asteroid) {
+    ctx.save();
+    ctx.translate(asteroid.realX, asteroid.realY);
+    ctx.rotate(asteroid.deg);
+
+    var spriteY,
+      spriteX = 256;
+    if (asteroid.state == 0) {
+      spriteY = 0;
+      spriteX = 0;
+    } else if (asteroid.state < 8) {
+      spriteY = 0;
+    } else if (asteroid.state < 16) {
+      spriteY = 256;
+    } else if (asteroid.state < 24) {
+      spriteY = 512;
+    } else {
+      spriteY = 768;
+    }
+
+    if (asteroid.state == 8 || asteroid.state == 16 || asteroid.state == 24) {
+      asteroid.stateX = 0;
+    }
+
+    ctx.drawImage(
+      spriteExplosion,
+      asteroid.stateX += spriteX,
+      spriteY,
+      256,
+      256,
+      - (asteroid.width / asteroid.size) / 2,
+      -(asteroid.height / asteroid.size) / 2,
+      asteroid.width / asteroid.size,
+      asteroid.height / asteroid.size
+    );
+    asteroid.state += 1;
+
+    if (asteroid.state == 31) {
+      asteroid.extinct = true;
+    }
+
+    ctx.restore();
+  }
+
+  function start() {
+    if (!gameOver) {
+      //Clear
+      ctx.clearRect(0, 0, cW, cH);
+      ctx.beginPath();
+
+      //Planet
+      planet();
+
+      //Player
+      _player();
+
+      if (playing) {
+        _asteroids();
+
+        ctx.font = "20px Verdana";
+        ctx.fillStyle = "white";
+        ctx.textBaseline = 'middle';
+        ctx.textAlign = "left";
+        ctx.fillText('Record: ' + record + '', 20, 30);
+
+        ctx.font = "40px Verdana";
+        ctx.fillStyle = "white";
+        ctx.strokeStyle = "black";
+        ctx.textAlign = "center";
+        ctx.textBaseline = 'middle';
+        ctx.strokeText('' + destroyed + '', cW / 2, cH / 2);
+        ctx.fillText('' + destroyed + '', cW / 2, cH / 2);
+
+      } else {
+        ctx.drawImage(sprite, 428, 12, 70, 70, cW / 2 - 35, cH / 2 - 35, 70, 70);
+      }
+    } else if (count < 1) {
+      count = 1;
+      ctx.fillStyle = 'rgba(0,0,0,0.75)';
+      ctx.rect(0, 0, cW, cH);
+      ctx.fill();
+
+      ctx.font = "60px Verdana";
+      ctx.fillStyle = "white";
+      ctx.textAlign = "center";
+      ctx.fillText("GAME OVER", cW / 2, cH / 2 - 150);
+
+      ctx.font = "20px Verdana";
+      ctx.fillStyle = "white";
+      ctx.textAlign = "center";
+      ctx.fillText("Total destroyed: " + destroyed, cW / 2, cH / 2 + 140);
+
+      record = destroyed > record ? destroyed : record;
+
+      ctx.font = "20px Verdana";
+      ctx.fillStyle = "white";
+      ctx.textAlign = "center";
+      ctx.fillText("RECORD: " + record, cW / 2, cH / 2 + 185);
+
+      ctx.drawImage(sprite, 500, 18, 70, 70, cW / 2 - 35, cH / 2 + 40, 70, 70);
+
+      canvas.removeAttribute('class');
+    }
+  }
+
+  function init() {
+    window.requestAnimationFrame(init);
+    start();
+  }
+
+  init();
+
+  //Utils
+  function random(from, to) {
+    return Math.floor(Math.random() * (to - from + 1)) + from;
+  }
+
+  if (~window.location.href.indexOf('full')) {
+    var full = document.getElementsByTagName('a');
+    full[0].setAttribute('style', 'display: none');
+  }
+}
